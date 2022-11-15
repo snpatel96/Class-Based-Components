afbf1c86dc0811a27cfb6b9aeafa0bf5d5359027
snehal patel <snpatel.dev@gmail.com>
2022-11-14 20:17:15 -0500

    #6 final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
+import './App.css';
 
 class App extends React.Component {
   state = { lat: null, errorMessage: '' };
@@ -12,7 +13,7 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -21,6 +22,10 @@ class App extends React.Component {
       return <SeasonDisplay latitude={this.state.lat} />;
     }
     return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
